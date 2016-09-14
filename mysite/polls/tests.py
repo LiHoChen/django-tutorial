833fdcc831976a64c802255a7f8a04e780395ee9
Leo Chen <took221@gmail.com>
2016-09-14 16:56:37 +0800

    Finished all tutorials

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,6 +6,14 @@ from django.urls import reverse
 
 from .models import Question
 
+def create_question(question_text, days):
+        """
+        Creates a question with the given `question_text` and published the
+        given number of `days` offset to now (negative for questions published
+        in the past, positive for questions that have yet to be published).
+        """
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionMethodTests(TestCase):
 
@@ -26,23 +34,13 @@ class QuestionMethodTests(TestCase):
 	    time = timezone.now() - datetime.timedelta(days=30)
 	    old_question = Question(pub_date=time)
 	    self.assertIs(old_question.was_published_recently(), False)
-
-	def test_was_published_recently_with_recent_question(self):
-	    """
-	    was_published_recently() should return True for questions whose
-	    pub_date is within the last day.
-	    """
-	    time = timezone.now() - datetime.timedelta(hours=1)
-	    recent_question = Question(pub_date=time)
-	    self.assertIs(recent_question.was_published_recently(), True)
-	def create_question(question_text, days):
-	    """
-	    Creates a question with the given `question_text` and published the
-	    given number of `days` offset to now (negative for questions published
-	    in the past, positive for questions that have yet to be published).
-	    """
-	    time = timezone.now() + datetime.timedelta(days=days)
-	    return Question.objects.create(question_text=question_text, pub_date=time)
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
 
 class QuestionViewTests(TestCase):
     def test_index_view_with_no_questions(self):
